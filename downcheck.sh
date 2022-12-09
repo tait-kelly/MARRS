cca8618e37324757c55a13834c5eb69bd7d564cd
tait-kelly <90005066+tait-kelly@users.noreply.github.com>
2022-12-09 13:35:54 -0500

    Create downcheck.sh

---
+++ downcheck.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+ls /home/sysadmin/Documents/errors | grep 172.17.6.40.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.40
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.50.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.50
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.60.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.60
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.70.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.70
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.80.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.80
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.90.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.90
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.120.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.120
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.130.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.130
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.140.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.140
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.160.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.160
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.170.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.170
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.180.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.180
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.190.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.190
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.200.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.200
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.210.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.210
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.220.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.220
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.230.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.230
+fi
+ls /home/sysadmin/Documents/errors | grep 172.17.6.240.txt
+if [ "$?" == "0" ]; then
+	./AVStatus.sh 172.17.6.240
+fi
+#ls /home/sysadmin/Documents/errors | grep 172.17.6.30.txt
+#if [ "$?" == "0" ]; then
+#	./AVStatus.sh 172.17.6.30
+#fi
+#ls /home/sysadmin/Documents/errors | grep 172.17.6.110.txt
+#if [ "$?" == "0" ]; then
+#	./AVStatus.sh 172.17.6.110
+#fi
